bc2c7974b239ef071363c9e09529bc6cda2c5649
Johnny <jengstrom27@gmail.com>
2022-03-09 15:18:29 +0100

    initial Commit

---
+++ src/components/header.js
@@ -0,0 +1,57 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Header = ({ siteTitle }) => (
+  <div>
+  <header
+    style={{
+      background: "#202123",
+      marginBottom: `1.45rem`,
+      margin: "0 15% 0 15%"
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <StaticImage
+      src="../images/nacka_rk_logo.png"
+      width={120}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+  <ul style={{backgroundColor: "#3B4A60", listStyleType: 'none', margin: "0 15% 0 15%", color: "white"}}>
+    <li>Hem</li>
+  </ul>
+  </div>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
