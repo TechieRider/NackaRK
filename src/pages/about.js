4548a5a5f08ff92333b3c1f4e722ca8d44117d29
Johnny <jengstrom27@gmail.com>
2022-03-23 22:30:04 +0100

    small change

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,14 +6,12 @@ import Layout from "../components/layout"
 
 const About = ({ siteTitle }) => {
     return (
-        <Layout>
             <div>
                 <h1 style={{color: 'white'}}>Om oss</h1>
                 <p>Intresserad av att börja lira? Gör en <a href="https://form.jotform.com/220694468297369">intresseanmälan här</a></p>
 
                 <p>Har du frågor? Maila oss på <a href="mailto:info@nackarugby.com">info@nackarugby.com</a></p>
             </div>
-  </Layout>
     )}
 
 export default About;
